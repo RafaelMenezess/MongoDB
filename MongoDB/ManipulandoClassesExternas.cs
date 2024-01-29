8cba506e522012ed560f26701611acffce19b2d8
Rafael Menezes <rafa.rcm@hotmail.com>
2024-01-29 16:49:15 -0300

    Encapsulando o acesso ao MongoDb

---
+++ MongoDB/ManipulandoClassesExternas.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MongoDB
+{
+    public class ManipulandoClassesExternas
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainAsync(args);
+            Console.WriteLine();
+            Console.WriteLine("Presione Enter");
+            Console.ReadLine();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            Livro livro = new Livro();
+            livro.Titulo = "Stars Wars";
+            livro.Autor = "Timothy Zahn";
+            livro.Ano = 2010;
+            livro.Paginas = 245;
+
+            List<string> listaAssunto = new List<string>();
+            listaAssunto.Add("Ficção Científica"); ;
+            listaAssunto.Add("Ação");
+            livro.Assunto = listaAssunto;
+
+            //acesso atraves da classe de conexao
+            var conexaoBiblioteca = new conectandoMongoDB();           
+
+            //incluindo documento
+            await conexaoBiblioteca.Livros.InsertOneAsync(livro);
+
+            Console.WriteLine("Documento Incluido");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MongoDB/conectandoMongoDB.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoDB
+{
+    class conectandoMongoDB
+    {
+        public const string STRING_DE_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "Biblioteca";
+        public const string NOME_DA_COLECAO = "Livros";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static conectandoMongoDB()
+        {
+            _client = new MongoClient(STRING_DE_CONEXAO);
+            _database = _client.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+        public IMongoCollection<Livro> Livros
+        {
+            get { return _database.GetCollection<Livro>(NOME_DA_COLECAO); }
+        }
+
+    }
+}
