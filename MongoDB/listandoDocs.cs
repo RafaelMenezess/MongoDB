8da1faba91d3f59303f87ecebc6c9275b4ca27e2
Rafael Menezes <rafa.rcm@hotmail.com>
2024-01-30 15:17:32 -0300

    Listando todos os livros

---
+++ MongoDB/listandoDocs.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace MongoDB
+{
+    public class listandoDocs
+    {
+        static void Main(string[] args)
+        {
+            //acesso atraves da classe de conexao
+            var conexaoBiblioteca = new conectandoMongoDB();
+
+            Console.WriteLine("Listando Documentos");
+            var listaLivros = conexaoBiblioteca.Livros.Find(new BsonDocument()).ToList();
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livro>());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
