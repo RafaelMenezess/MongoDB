4bc47002784888fd1fa71bcc60203ec80b5c8dd5
Rafael Menezes <rafa.rcm@hotmail.com>
2024-01-30 15:06:44 -0300

    Incluindo muitos livros

--- MongoDB/Livro.cs
+++ MongoDB/Livro.cs
@@ -14,4 +14,25 @@ namespace MongoDB
         public int Paginas { get; set; }
         public List<string> Assunto { get; set; }
     }
+
+    public class valoresLivro
+    {
+        public static Livro incluiValoresLivro(string Titulo, string Autor, int Ano, int Paginas, string assuntos)
+        {
+            Livro livro = new Livro();
+            livro.Titulo = Titulo;
+            livro.Autor = Autor;
+            livro.Ano = Ano;
+            livro.Paginas = Paginas;
+            string[] vetAssunto = assuntos.Split(',');
+            List<string> vetAssunto2 = new List<string>();
+            for (int i = 0; i < vetAssunto.Length; i++)
+            {
+                vetAssunto2.Add(vetAssunto[i].Trim());
+            }
+            livro.Assunto = vetAssunto2;
+
+            return livro;
+        }
+    }
 }
---
+++ MongoDB/usandoValoresLivros.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace MongoDB
+{
+    public class usandoValoresLivros
+    {
+        static void Main(string[] args)
+        {
+            //acesso atraves da classe de conexao
+            var conexaoBiblioteca = new conectandoMongoDB();
+
+            Livro livro = new Livro();
+            livro = valoresLivro.incluiValoresLivro("O Senhor dos Aneis", "J R R Token", 1948, 1956, "Fantasia, Ação");
+
+            //incluindo documento
+            conexaoBiblioteca.Livros.InsertOne(livro);
+
+            Console.WriteLine("Documento Incluido");
+
+            Console.ReadLine();
+        }
+    }
+}
