fbe6d9837c449c93e7b84ad117b214c9c0e02650
Rafael Menezes <rafa.rcm@hotmail.com>
2024-02-03 07:28:14 -0300

    Excluindo docs

---
+++ MongoDB/excluindoDoc.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+
+namespace MongoDB
+{
+    public class excluindoDoc
+    {
+        static void Main(string[] args)
+        {
+            //acesso atraves da classe de conexao
+            var conexaoBiblioteca = new conectandoMongoDB();
+
+            Console.WriteLine("Bucas os Livros George R Martin ");
+            var contrutor = Builders<Livro>.Filter;
+            var condicao = contrutor.Eq(x => x.Autor, "George R Martin");
+
+            var listaLivros = conexaoBiblioteca.Livros.Find(condicao).ToList();
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livro>());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fim da lista");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Excluindo os livros");
+            conexaoBiblioteca.Livros.DeleteMany(condicao);
+
+            listaLivros = conexaoBiblioteca.Livros.Find(condicao).ToList();
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livro>());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fim da lista");
+            Console.WriteLine("");
+            Console.WriteLine("");           
+
+            Console.ReadLine();
+        }
+    }
+}
