b28ff3fd32fbf3e58df190e44d7e94e91d415145
Rafael Menezes <rafa.rcm@hotmail.com>
2024-02-03 06:58:15 -0300

    Usando SortBy e Limit

---
+++ MongoDB/listandoDocPorOrdem.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+
+namespace MongoDB
+{
+    public class listandoDocPorOrdem
+    {
+        static void Main(string[] args)
+        {
+            //acesso atraves da classe de conexao
+            var conexaoBiblioteca = new conectandoMongoDB();
+
+
+            Console.WriteLine("Listando Documentos Machado de Assis mais de 100 páiginas");
+            var contrutor = Builders<Livro>.Filter;
+            var condicao = contrutor.Gt(x => x.Paginas, 100);
+
+            var listaLivros = conexaoBiblioteca.Livros.Find(condicao).SortBy(x => x.Titulo).Limit(5).ToList();
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livro>());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fim da lista");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
