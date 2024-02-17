4952dba2a1ccb0fa8a36894fe0e5f2bfee691704
Rafael Menezes <rafa.rcm@hotmail.com>
2024-02-17 15:27:13 -0300

    Criando a classe Aeroporto

---
+++ MongoDB/Aeroporto.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace MongoDB
+{
+    public class Aeroporto
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates> Loc { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Code { get; set; }
+    }
+}
